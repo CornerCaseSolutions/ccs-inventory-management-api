fa9c82860cbf5f4d8e3526cf9b22f1d6c6fa2353
Darius  Ramavarapu <dariusramavarapu@Dariuss-MBP.hsd1.pa.comcast.net>
2024-06-09 00:22:09 -0400

    Creates service for reads and updates for clothingService

---
+++ InventoryManagement/src/main/java/com/ccs/inventorymanagement/service/ItemService.java
@@ -0,0 +1,21 @@
+package com.ccs.inventorymanagement.service;
+
+import com.ccs.inventorymanagement.domain.Item;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface ItemService<T extends Item> {
+
+    Flux<T> findAll();
+
+    Mono<T> findById(UUID id);
+
+    Mono<T> create(T item);
+
+    Mono<T> update(T item);
+
+    Mono<T> delete(UUID id);
+
+}
