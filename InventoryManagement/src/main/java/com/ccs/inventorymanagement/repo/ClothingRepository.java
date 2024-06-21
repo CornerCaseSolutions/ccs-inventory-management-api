27e53cafbcd559c061d02c9f92d75e4db3e0c06b
jmusone <jacobmusone@gmail.com>
2024-06-21 13:26:33 -0400

    Updated delete to be soft delete
    I couldn't figure out how to do it just in the service file so I figured this could be a working alternative.

--- InventoryManagement/src/main/java/com/ccs/inventorymanagement/repo/ClothingRepository.java
+++ InventoryManagement/src/main/java/com/ccs/inventorymanagement/repo/ClothingRepository.java
@@ -1,10 +1,14 @@
 package com.ccs.inventorymanagement.repo;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface ClothingRepository extends R2dbcRepository<ClothingEntity, UUID> {
+    @Query("UPDATE clothing SET status = 'ISSUED' WHERE id = $0")
+    Mono<Void> deleteByID(UUID id);
 }
